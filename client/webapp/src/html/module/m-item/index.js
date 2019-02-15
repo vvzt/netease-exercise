b49b098292ec5a957c79a469089cc1134b547a2c
vzt7 <vvzt666666@foxmail.com>
2019-02-16 01:42:21 +0800

    fix localstorage (setdata no ok)

--- client/webapp/src/html/module/m-item/index.js
+++ client/webapp/src/html/module/m-item/index.js
@@ -45,7 +45,7 @@ NEJ.define([
       this.__onAction._$bind(this)
     )
     _event._$addEvent(
-      _btns[0], // delete
+      _btns[0], // delete btn
       'click',
       this.__onAction._$bind(this)
     )
@@ -54,22 +54,20 @@ NEJ.define([
   // 刷新
   _pro.__doRefresh = function(_data){
     this.__data = _data
-    this.__nindex.id = _data.id
+    // this.__nindex.id = _data.id
     this.__ntitle.innerHTML = _data.title
     
+    // 切换 class
     _element._$addClassName(this.__nindex, 'z-index-completed-' + (_data.isCompleted ? 'ok' : 'no'))
     _element._$addClassName(this.__ntitle, 'z-item-completed-' + (_data.isCompleted ? 'ok' : 'no'))
-    // // 子评论列表
-    // if (!!_data.replies){
-    //     // 子评论构造同当前评论项
-    //     this.__items = _t._$getItemTemplate(
-    //         _data.replies,this.constructor,{
-    //             parent:this.__body,
-    //             onreply:this.__onReply._$bind(this),
-    //             ondelete:this.__onDelete._$bind(this)
-    //         }
-    //     )
-    // }
+
+    // 存入 localstorage
+    _storage._$setDataInStorage('todo' + _data.id, _data)
+    // todos id索引数组
+    this.__todos = _storage._$getDataInStorage('todos')
+    this.__todos.push(_data.id)
+    _storage._$setDataInStorage('todos', this.__todos)
+    console.log(777)
   }
 
   // 操作
@@ -79,7 +77,6 @@ NEJ.define([
     if(_actionEvent.type === 'click') {
       switch(_node) {
         case this.__nindex: {
-          console.log('click fn:', this.__data)
           // 通过完成的状态切换 class
           var _nextStatus = !this.__data.isCompleted
           
@@ -87,14 +84,21 @@ NEJ.define([
           this.__data.isCompleted = _nextStatus
           break
         }
-        case this.__nedit: {
-          // 编辑
-          
-          console.log('edit')
-          break
-        }
         case this.__ndel: {
-          // 删除
+          // del
+          var _id = this.__data.id
+
+          // 更新 localstorage todos
+          var _idIndex
+          this.__todos.forEach(function(id, i) {
+            if(id === _id) _idIndex = i
+          })
+          console.log(_idIndex, this.__todos[_idIndex])
+          if(_idIndex > -1) this.__todos.splice(_idIndex, 1)
+          _storage._$delDataInStorage('todo' + _id)
+          _storage._$setDataInStorage('todos', this.__todos)
+          console.log('todo del', _idIndex, this.__data.title)
+
           this.__destroy()
           break
         }
@@ -104,8 +108,8 @@ NEJ.define([
 
   _pro._$setCompleted = function(_nextStatus) {
     if(_nextStatus) {
-      _element._$replaceClassName(this.__nindex,'z-index-completed-no', 'z-index-completed-ok')
-      _element._$replaceClassName(this.__ntitle,'z-item-completed-no', 'z-item-completed-ok')
+      _element._$replaceClassName(this.__nindex, 'z-index-completed-no', 'z-index-completed-ok')
+      _element._$replaceClassName(this.__ntitle, 'z-item-completed-no', 'z-item-completed-ok')
     } else {
       _element._$replaceClassName(this.__nindex, 'z-index-completed-ok', 'z-index-completed-no')
       _element._$replaceClassName(this.__ntitle, 'z-item-completed-ok', 'z-item-completed-no')
--- client/webapp/src/html/module/m-todo/index.js
+++ client/webapp/src/html/module/m-todo/index.js
@@ -8,21 +8,23 @@ NEJ.define([
   'util/cache/storage',
   '../m-item/index.js'
 ], function(_klass, _element, _event, _module, _tpl, _jst, _storage, _mlist, _p) {
-  
+
   _p._$$ModuleList = _klass._$klass()
   var _pro = _p._$$ModuleList._$extend(_module._$$ModuleAbstract)
-  
+
   _pro.__doBuild = function() {
     this.__body = _element._$html2node(_tpl._$getTextTemplate('m-todo'))
 
-    this.__todoStorage = _storage._$getDataInStorageWithDefault('todos', {})
-
+    var todosInStorage = _storage._$getDataInStorageWithDefault('todos', [])
+    var defaultTodos = todosInStorage.map(function(todoId) {
+      return _storage._$getDataInStorage(todoId)
+    }) || [
+      { id: 'asfasf', title: 'true', isCompleted: true },
+      { id: 'asjdiaw', title: 'false', isCompleted: false }
+    ]
 
     var _todoList = _tpl._$getItemTemplate(
-      [
-        { id: 'asfasf', title: 'true', isCompleted: true },
-        { id: 'asjdiaw', title: 'false', isCompleted: false }
-      ],
+      defaultTodos,
       _mlist._$$ModuleItem,
       {
         parent: 'm-list',
@@ -32,50 +34,34 @@ NEJ.define([
     )
     this.__list = _todoList
 
-    // var _templateSeed = _jst._$add('m-list-template')
-
-    // _jst._$get(_templateSeed, {
-    //   test: 123
-    // })
-    
-    // _mlist._$$ModuleList._$allocate({
-    //   parent: this.__body,
-    //   clazz: 'ttt'
-    // })
-
-
-    // _jst._$render('m-list', _todoList, {
-    //   test: 222
-    // })
-    
-
   }
 
   _pro.__onShow = function(_options) {
     this.__super(_options)
     var that = this
     var el_input = _element._$getByClassName(this.__body, 'u-input')[0]
+    
     _event._$addEvent(el_input, 'enter', function(_actionEvent) {
+      // 新增 todo
       var val = this.value
       this.value = ''
       that.__events.onrefresh({
-        item: {
-          id: '',
+        todo: {
+          id: 'tempid-' + Date.now(),
           title: val
         }
       })
-      // onenter todo 
     }, false)
   }
 
   _pro.__onRefresh = function(_data) {
     this.__super(_data)
     console.log(1, 'onrefresh', _data)
-    if(_data.item) {
+    if(_data.todo) {
       // add
       _tpl._$getItemTemplate(
         [
-          _data.item
+          _data.todo
         ],
         _mlist._$$ModuleItem,
         {
